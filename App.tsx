4635682f09554711ac7c58017be136eb9443489d
Rafael Cena <rafael.cena@hotmail.com>
2022-10-23 15:38:18 -0300

    status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components/native'; 
+import { StatusBar } from 'react-native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
@@ -11,6 +12,12 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+      barStyle='light-content'
+      backgroundColor='transparent'
+      translucent
+      
+      />
       { !fontsLoaded ? <Groups/> : <Loading /> }
     </ThemeProvider>
   );

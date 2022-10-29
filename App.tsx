e28413cc990c6a10306f926c37f6ca62b7dcfbcc
Rafael Cena <rafael.cena@hotmail.com>
2022-10-29 14:31:28 -0300

    new group

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'react-native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
-
+import { NewGroup } from '@screens/NewGroup';
 import {Groups} from "@screens/Groups";
 import { Loading } from '@components/Loading';
 
@@ -12,13 +12,15 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
+      <>
       <StatusBar 
       barStyle='light-content'
       backgroundColor='transparent'
       translucent
       
       />
-      { fontsLoaded ? <Groups/> : <Loading /> }
+      { fontsLoaded ? <NewGroup/> : <Loading /> }
+      </>
     </ThemeProvider>
   );
 }

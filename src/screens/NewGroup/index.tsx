06bc5944c08af15febeeef895315ac66bb6ad5e5
Rafael Cena <rafael.cena@hotmail.com>
2022-10-30 13:46:27 -0300

    tratando erros

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,9 @@
 import { Container, Content, Icon } from "./styles";
 import { Input } from "@components/Input";
 import { Header } from "@components/Header";
+import { Alert } from "react-native";
+
+import { AppError } from "@utils/AppError";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { useNavigation } from "@react-navigation/native";
@@ -11,14 +14,24 @@ import { groupCreate } from "@storage/group/groupCreate";
 export function NewGroup() {
   const [ group, setGroup ] = useState('')
   const navigation = useNavigation()
-  
+
 
   async function handleNew() {
       try {
+
+        if(group.trim().length === 0) {
+          return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+        }
+
         await groupCreate(group);
         navigation.navigate('players', { group })
       } catch (error) {
-        console.log(error)
+        if(error instanceof AppError) {
+          Alert.alert('Novo Grupo', error.message);
+        } else {
+          Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+          console.log(error);
+        }
       }
   }
   return (
